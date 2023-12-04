8aaa58f9874254680d95e81355566ac536faa4e6
EC2 Default User <ec2-user@ip-172-31-58-110.ec2.internal>
2023-12-04 17:00:13 +0900

    おはよう

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,7 +14,8 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}
+                    <h2 class='title'><a href="/posts/{{ $post->id }}">
+                    {{ $post->title }}</a></h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
