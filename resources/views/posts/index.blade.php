00cfe094e4758a714ba7255d63b18d3488ee6f61
EC2 Default User <ec2-user@ip-172-31-58-110.ec2.internal>
2023-12-04 23:27:47 +0900

    delete

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,19 +3,36 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
+        <a href="/posts/create">create</a>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <a href="/posts/{{ $post->id }}"><h2< class='title'>{{ $post->title }}</h2></a>
+                    <h2 class='title'><a href="/posts/{{ $post->id }}">
+                    {{ $post->title }}</a></h2>
                     <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
                 </div>
             @endforeach
         </div>
         <div class='paginate'>{{ $posts->links()}}</div>
+        <script>
+            function deletePost(id) {
+                'use strict'
+                
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+            }
+        }
+        </script>
     </body>
 </html>
