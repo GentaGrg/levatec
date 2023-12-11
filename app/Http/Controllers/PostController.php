580ff51dc5ec15412b4f454f646e513737ad88cf
EC2 Default User <ec2-user@ip-172-31-58-110.ec2.internal>
2023-12-11 16:49:12 +0900

    9-4完成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,41 +11,18 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(5)]);  
-    }
-    
-    public function show(Post $post)
-    {
-        return view('posts.show')->with(['post' => $post]);
-    }
-    
-    public function create(Category $category)
-    {
-        return view('posts.create')->with(['categories' => $category->get()]);
-    }
-    
-    public function store(PostRequest $request, Post $post)
-    {
-        $input = $request->input('post');
-        $post->fill($input)->save();
-        return redirect('/posts/' . $post->id);
-    }
-    
-    public function edit(Post $post)
-    {
-        return view('posts.edit')->with(['post' => $post]);
-    }
-    
-    public function update(PostRequest $request, Post $post)
-    {
-        $input_post = $request->input('post');
-        $post->fill($input_post)->save();
-        return redirect('/posts/' . $post->id);
-    }
-    
-    public function delete(Post $post)
-    {
-        $post->delete();
-        return redirect('/');
+        $client = new \GuzzleHttp\Client();
+        $url = 'https://teratail.com/api/v1/questions';
+        $response = $client->request(
+            'GET',
+            $url,
+            ['Bearer' => config('services.teratail.token')]
+        );
+        $questions = json_decode($response->getBody(), true);
+        return view('posts.index')->with([
+            'posts' => $post->getPaginateByLimit(),
+            'questions' => $questions['questions'],
+        ]);
+        
     }
 }
